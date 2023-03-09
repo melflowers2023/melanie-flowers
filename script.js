d59fd19b12113a6721f2e13880ee793d823af136
melflowers2023 <91751908+melflowers2023@users.noreply.github.com>
2023-03-09 14:52:04 -0600

    apps added, gradient nav, typewriter bio

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ var TxtType = function(el, toRotate, period) {
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
         var that = this;
-        var delta = 200 - Math.random() * 100;
+        var delta = 100 - Math.random() * 100;
 
         if (this.isDeleting) { delta /= 2; }
 
@@ -31,7 +31,7 @@ var TxtType = function(el, toRotate, period) {
         } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
-        delta = 500;
+        delta = 250;
         }
 
         setTimeout(function() {
@@ -48,9 +48,33 @@ var TxtType = function(el, toRotate, period) {
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
+        setInterval(change, 3000);
+        setInterval(change2, 3000);
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+
+let images = ['C:/Users/melfl/OneDrive/Documents/melanie-flowers/di2.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/di3.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/dihome.PNG'];
+let index = 0;
+const imgElement = document.querySelector('#mainPhoto');
+
+function change() {
+   imgElement.src = images[index];
+   index > 1 ? index = 0 : index++;
+}
+
+let images2 = ['C:/Users/melfl/OneDrive/Documents/melanie-flowers/zd2.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/zd3.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/zdhome.PNG'];
+let index2 = 0;
+const imgElement2 = document.querySelector('#mainPhoto2');
+
+function change2() {
+   imgElement2.src = images2[index2];
+   index2 > 1 ? index2 = 0 : index2++;
+}
+
+// window.onload = function () {
+//     setInterval(change, 5000);
+// };
