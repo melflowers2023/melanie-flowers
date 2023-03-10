15369e133a11c02ac0c35d0c683b5648278c8ff4
melflowers2023 <91751908+melflowers2023@users.noreply.github.com>
2023-03-09 19:51:08 -0600

    ready to add skills

--- script.js
+++ script.js
@@ -48,8 +48,15 @@ var TxtType = function(el, toRotate, period) {
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
-        setInterval(change, 3000);
-        setInterval(change2, 3000);
+        function change() {
+            imgElement.src = photos[index];
+            index > 1 ? index = 0 : index++;
+         }
+
+
+        console.log(interval)
+        setInterval(change, interval);
+        
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
@@ -57,24 +64,54 @@ var TxtType = function(el, toRotate, period) {
         document.body.appendChild(css);
     };
 
-let images = ['C:/Users/melfl/OneDrive/Documents/melanie-flowers/di2.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/di3.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/dihome.PNG'];
+let interval = 4000;
+let images = ['C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/di2.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/di3.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/dihome.PNG'];
 let index = 0;
 const imgElement = document.querySelector('#mainPhoto');
 
 function change() {
-   imgElement.src = images[index];
+    imgElement.className = size;
+   imgElement.src = photos[index];
+   
    index > 1 ? index = 0 : index++;
+   console
 }
 
-let images2 = ['C:/Users/melfl/OneDrive/Documents/melanie-flowers/zd2.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/zd3.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/zdhome.PNG'];
-let index2 = 0;
-const imgElement2 = document.querySelector('#mainPhoto2');
-
-function change2() {
-   imgElement2.src = images2[index2];
-   index2 > 1 ? index2 = 0 : index2++;
-}
+let images2 = ['C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/zd2.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/zd3.PNG', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/zdhome.PNG'];
+let images3 = ['C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/ig3.gif', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/ig4.gif', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/ig1.gif', 'C:/Users/melfl/OneDrive/Documents/melanie-flowers/sources/ig2.gif']
 
 // window.onload = function () {
 //     setInterval(change, 5000);
 // };
+let size = 'di1';
+let photos = images;
+function choose(choice) {
+    console.log(choice)
+    if (choice == 'zd') {
+        photos = images2;
+        index = 2;
+        clearInterval();
+        setInterval(change, 3000);
+        size = 'di1';
+    }
+    else if (choice == 'ig') {
+        photos = images3;
+        index = 2;
+        console.log(interval)
+        interval = 7000;
+        clearInterval();
+        setInterval(change, 7000);
+        console.log(interval);
+        size = 'ig1';
+        
+    }
+    else {
+        photos = images;
+        index = 2;
+        clearInterval();
+        setInterval(change, 3000);
+        size = 'di1';
+    }
+    
+    
+}
