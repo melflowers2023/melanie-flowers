a8f43d968dbaae17b6cfe50d14d85fdf8a437d82
melflowers2023 <91751908+melflowers2023@users.noreply.github.com>
2023-03-29 14:49:23 -0500

    edited app descriptions and improved css

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
+// var TxtType = function(el, toRotate, period) {
+//         this.toRotate = toRotate;
+//         this.el = el;
+//         this.loopNum = 0;
+//         this.period = parseInt(period, 10) || 2000;
+//         this.txt = '';
+//         this.tick();
+//         this.isDeleting = false;
+//     };
 
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+//     TxtType.prototype.tick = function() {
+//         var i = this.loopNum % this.toRotate.length;
+//         var fullTxt = this.toRotate[i];
 
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
+//         if (this.isDeleting) {
+//         this.txt = fullTxt.substring(0, this.txt.length - 1);
+//         } else {
+//         this.txt = fullTxt.substring(0, this.txt.length + 1);
+//         }
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+//         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-        var that = this;
-        var delta = 100 - Math.random() * 100;
+//         var that = this;
+//         var delta = 100 - Math.random() * 100;
 
-        if (this.isDeleting) { delta /= 2; }
+//         if (this.isDeleting) { delta /= 2; }
 
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 250;
-        }
+//         if (!this.isDeleting && this.txt === fullTxt) {
+//         delta = this.period;
+//         this.isDeleting = true;
+//         } else if (this.isDeleting && this.txt === '') {
+//         this.isDeleting = false;
+//         this.loopNum++;
+//         delta = 250;
+//         }
 
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
+//         setTimeout(function() {
+//         that.tick();
+//         }, delta);
+//     };
 
     window.onload = function() {
         var elements = document.getElementsByClassName('typewrite');
@@ -118,7 +118,7 @@ function change() {
 }
 
 let images2 = ['sources/zdhome.PNG', 'sources/zd2.PNG', 'sources/zd3.PNG'];
-let images3 = ['sources/cb1.PNG', 'sources/ig4.gif', 'sources/ig1.gif', 'sources/ig2.gif']
+let images3 = ['sources/ig4.gif', 'sources/ig1.gif', 'sources/ig2.gif']
 let images4 = ['sources/cb1.PNG', 'sources/cb2.PNG', 'sources/cb3.PNG'];
 
 let size = 'di1';
